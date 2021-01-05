3db14332140422fee2b33c832854ec15cacd7a36
ACER_Nine <ach_-_nai@rmutsvmail.com>
2021-01-05 16:02:15 +0700

    hw_lab3

---
+++ src/HomeWork/HW_Login.java
@@ -0,0 +1,33 @@
+package HomeWork;
+
+import java.util.Scanner;
+
+public class HW_Login {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+
+        while(count<3){
+            System.out.print("Username : ");
+            String usr = sc.nextLine();
+
+            System.out.print("Password : ");
+            String pwd = sc.nextLine();
+            if(usr.equals("admin") && pwd.equals("saiyai"))
+            {
+                System.out.println("Welcome to MT Website.");
+                break;
+            }
+            else {
+                System.out.println("username or password not correct." );
+                count++;
+            }
+            if(count==3)
+            {
+                System.out.println("You account has been locked.Please, contect admin.");
+            }
+        }//while
+
+
+    }//main
+}//class
